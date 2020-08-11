aabf5798dffda0904007b06a317d7634c86cd82e
Mwikala Kangwa <hello@mwikala.co.uk>
2020-08-11 16:22:55 +0100

    Added swiping to slideshows

--- js/scripts.js
+++ js/scripts.js
@@ -25,37 +25,74 @@ function initialiseSlider() {
     $(".sliderTotal").text(slides.length);
 
     // when the slider controls, inserted earlier are clicked
-    $('#slidePrev').click(function(){
-        if(slidePos !== 0) {
+    $('#slidePrev').click(function () {
+        if (slidePos !== 0) {
             $(slides[slidePos]).addClass("hide");
             slidePos--;
             updateSliderCounter(slidePos);
         }
     });
 
-    $('#slideNext').click(function(){
-        if(slidePos !== slides.length - 1) {
+    $('#slideNext').click(function () {
+        if (slidePos !== slides.length - 1) {
             slidePos++;
             $(slides[slidePos]).removeClass("hide");
             updateSliderCounter(slidePos);
         }
     });
 
+    let touchstartX = 0;
+    let touchendX = 0;
+    let gestureZone = document.getElementsByClassName('middle');
+
+    if (gestureZone.length > 0) {
+        gestureZone[0].addEventListener('touchstart', function (event) {
+            touchstartX = event.changedTouches[0].screenX;
+            touchstartY = event.changedTouches[0].screenY;
+        }, false);
+
+        gestureZone[0].addEventListener('touchend', function (event) {
+            touchendX = event.changedTouches[0].screenX;
+            touchendY = event.changedTouches[0].screenY;
+            handleGesture();
+        }, false);
+
+        function handleGesture() {
+            if (touchendX < touchstartX) {
+                if (slidePos !== slides.length - 1) {
+                    slidePos++;
+                    $(slides[slidePos]).removeClass("hide");
+                    updateSliderCounter(slidePos);
+                }
+            }
+
+            if (touchendX > touchstartX) {
+                if (slidePos !== 0) {
+                    $(slides[slidePos]).addClass("hide");
+                    slidePos--;
+                    updateSliderCounter(slidePos);
+                }
+            }
+        }
+    }
+
 }
 
-$(function(){
+$(function () {
 
     // make hamburger open the menu
-    $("#menu-button, .overlay").click(function(){
+    $("#menu-button, .overlay").click(function () {
         $('body').toggleClass('menu-active');
     });
 
     // window on load waits for images to be loaded as well, but must be inside jquery ready to allow for query to work;
-    $(window).on("load", function() {
+    $(window).on("load", function () {
         // initialise slider    
         initialiseSlider();
     });
 
 });
 
+function registerSwipeEvents(gestureZone) {
 
+}
